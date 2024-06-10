d5e591331595bc0d46a292c59bb8f9ac7b075a2d
**Raquel** <00001120451875sp@al.educacao.sp.gov.br>
2024-06-10 14:58:56 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,47 @@
+straRaquete(x,y) {
+    rect(x, y, raqueteComprimento, raqueteAltura);
+  }
+  
+  function movimentaMinhaRaquete() {
+    if(keyIsDown(UP_ARROW)) {
+      yRaquete -= 10;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+      yRaquete += 10;
+    }
+  }
+  
+  function verificaColisaoRaquete() {
+    if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete) {
+      velocidadeXBolinha *= -1;
+    }
+  }
+  
+  function verificaColisaoRaquete(x,y) {
+    colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+    if(colidiu) {
+      velocidadeXBolinha *= -1;
+    }
+  }
+  
+  function movimentaRaqueteOponente() {
+    velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
+    yRaqueteOponente += velocidadeYOponente;
+  }
+  
+  function incluiPlacar() {
+    fill(255);
+    text(meusPontos, 278, 26);
+    text(pontosDoOponente, 321, 26);
+  }
+  
+  function marcaPonto() {
+    if (xBolinha > 590) {
+      meusPontos += 1;
+    }
+    if (xBolinha < 10) {
+      pontosDoOponente += 1;
+    }
+  }
+
+  
